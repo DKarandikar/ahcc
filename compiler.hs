f7c932185bdf36a818adcd26601eced5392804dd
DKarandikar <daniel.karandikar@zoho.com>
2020-06-14 23:46:31 +0100

    Add 'test' mode to compiler to print various stages
    Also delete compiler in anticipation of ignoring it

--- compiler.hs
+++ compiler.hs
@@ -140,17 +140,32 @@ evaluate (ExprNode x) = show x
 
 ---- main ----
 
+doNormal :: String -> IO()
+doNormal s = do
+   handle <- openFile s ReadMode
+   contents <- hGetContents handle
+   outputFile <- openFile "assembly.s" WriteMode
+   hPutStrLn outputFile (evaluate $ parse $ lexor contents)
+   hClose outputFile
+   hClose handle
+
+doTest :: String -> IO()
+doTest s = do
+   handle <- openFile s ReadMode
+   contents <- hGetContents handle
+
+   print contents
+   print (lexor contents)
+   print (parse $ lexor contents)
+   print (evaluate $ parse $ lexor contents)
+
+   hClose handle
+
+
 main :: IO()
 main = do  
-        let list = []
         args <- getArgs
-        handle <- openFile (head args) ReadMode
-        contents <- hGetContents handle
-      --   print contents
-      --   print (lexor contents)
-      --   print (parse $ lexor contents)
-      --   print (evaluate $ parse $ lexor contents)
-        goodbyeFile <- openFile "assembly.s" WriteMode
-        hPutStrLn goodbyeFile (evaluate $ parse $ lexor contents)
-        hClose goodbyeFile
-        hClose handle 
+        case args of 
+           [] -> error "Please provide path to .c file"
+           a : [] -> doNormal a
+           a : b : [] -> doTest a
