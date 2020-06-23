1170b102d7d86c3c7ca9d00f5ae19cb1427a1395
DKarandikar <daniel.karandikar@zoho.com>
2020-06-23 23:57:00 +0100

    Refactor evaluator a bit to make the logic clearer
    Do this by creating some helper methods for the generated assembly

--- src/compiler/Evaluator.hs
+++ src/compiler/Evaluator.hs
@@ -2,6 +2,9 @@ module Evaluator(evaluate) where
 
 import Parser (Tree(..))
 
+rax = "%rax"
+rcx = "%rcx"
+
 evaluate :: Tree -> String
 evaluate (FuncNode fname tree) = 
     let x = evaluate tree 
@@ -13,18 +16,35 @@ evaluate (ReturnNode tree) =
     let x = evaluate tree 
     in x ++ "    ret"
 
-evaluate (ConstNode x) =  "    movq   $" ++ (show x) ++ ", %rax\n"
+evaluate (ConstNode x) =  move ('$': (show x)) rax
 evaluate (UnOpNode op tree)
-    | op == "-" = evaluate tree ++ "    neg    %rax\n"
-    | op == "~" = evaluate tree ++ "    not    %rax\n"
-    | op == "!" = evaluate tree ++ "    cmpq   $0, %rax\n    movq   $0, %rax\n    sete    %al\n"
+    | op == "-" = evaluate tree ++ (doUnOp "neg" rax)
+    | op == "~" = evaluate tree ++ (doUnOp "not" rax)
+    | op == "!" = evaluate tree ++ (doBinOp "cmpq" "$0" rax) ++ (move "$0" rax) ++ (doUnOp "sete" "%al")
     | otherwise = error "Invalid unary operation: " ++ op
 
-
 evaluate (BinOpNode op tree tree')
-    | op == "+" = evaluate tree ++ "    push %rax\n" ++ (evaluate tree') ++ "    pop %rcx\n    addq %rcx, %rax\n"
-    | op == "-" = evaluate tree ++ "    push %rax\n" ++ (evaluate tree') ++ "    pop %rcx\n    subq %rax, %rcx\n    movq %rcx, %rax\n"
-    | op == "/" = evaluate tree ++ "    push %rax\n" ++ (evaluate tree') ++ "    movq %rax, %rcx\n    pop %rax\n    cqto\n    idivq %rcx\n"
-    | op == "*" = evaluate tree ++ "    push %rax\n" ++ (evaluate tree') ++ "    pop %rcx\n    imul %rcx, %rax\n"
+    | op == "+" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "addq" rcx rax)
+    | op == "-" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "subq" rax rcx) ++ (move rcx rax)
+    | op == "/" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (move rax rcx) ++ (pop rax) ++ (doOp "cqto") ++ (doUnOp "idivq" rcx)
+    | op == "*" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "imul" rcx rax)
     | otherwise  = error "Invalid binary operation: " ++ op
     
+
+move :: String -> String -> String
+move v1 v2 = "    movq " ++ v1 ++ ", " ++ v2 ++ "\n"
+
+push :: String -> String
+push v = "    push " ++ v ++ "\n"
+
+pop :: String -> String
+pop v = "    pop " ++ v ++ "\n"
+
+doBinOp:: String -> String -> String -> String
+doBinOp op v1 v2 = "    " ++ op ++ " " ++ v1 ++ ", " ++ v2 ++ "\n"
+
+doUnOp:: String -> String -> String
+doUnOp op v = "    " ++ op ++ " " ++ v ++ "\n"
+
+doOp:: String -> String
+doOp op = "    " ++ op ++ "\n"
