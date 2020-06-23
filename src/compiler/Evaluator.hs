48079b20469a54e527410b465c97882b4e778120
DKarandikar <daniel.karandikar@zoho.com>
2020-06-24 00:09:44 +0100

    Remove chains of ++ in favour of concat and a list
    I find this clearer to read currently

--- src/compiler/Evaluator.hs
+++ src/compiler/Evaluator.hs
@@ -18,33 +18,33 @@ evaluate (ReturnNode tree) =
 
 evaluate (ConstNode x) =  move ('$': (show x)) rax
 evaluate (UnOpNode op tree)
-    | op == "-" = evaluate tree ++ (doUnOp "neg" rax)
-    | op == "~" = evaluate tree ++ (doUnOp "not" rax)
-    | op == "!" = evaluate tree ++ (doBinOp "cmpq" "$0" rax) ++ (move "$0" rax) ++ (doUnOp "sete" "%al")
+    | op == "-" = concat [evaluate tree, doUnOp "neg" rax]
+    | op == "~" = concat [evaluate tree, doUnOp "not" rax]
+    | op == "!" = concat [evaluate tree, doBinOp "cmpq" "$0" rax, move "$0" rax, doUnOp "sete" "%al"]
     | otherwise = error "Invalid unary operation: " ++ op
 
 evaluate (BinOpNode op tree tree')
-    | op == "+" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "addq" rcx rax)
-    | op == "-" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "subq" rax rcx) ++ (move rcx rax)
-    | op == "/" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (move rax rcx) ++ (pop rax) ++ (doOp "cqto") ++ (doUnOp "idivq" rcx)
-    | op == "*" = evaluate tree ++ (push rax) ++ (evaluate tree') ++ (pop rcx) ++ (doBinOp "imul" rcx rax)
+    | op == "+" = concat [evaluate tree, push rax, evaluate tree', pop rcx, doBinOp "addq" rcx rax]
+    | op == "-" = concat [evaluate tree, push rax, evaluate tree', pop rcx, doBinOp "subq" rax rcx, move rcx rax]
+    | op == "/" = concat [evaluate tree, push rax, evaluate tree', move rax rcx, pop rax, doOp "cqto", doUnOp "idivq" rcx]
+    | op == "*" = concat [evaluate tree, push rax, evaluate tree', pop rcx, doBinOp "imul" rcx rax]
     | otherwise  = error "Invalid binary operation: " ++ op
     
 
 move :: String -> String -> String
-move v1 v2 = "    movq " ++ v1 ++ ", " ++ v2 ++ "\n"
+move v1 v2 = concat ["    movq " ++ v1, ", ", v2, "\n"]
 
 push :: String -> String
-push v = "    push " ++ v ++ "\n"
+push v = concat ["    push ", v, "\n"]
 
 pop :: String -> String
-pop v = "    pop " ++ v ++ "\n"
+pop v = concat ["    pop ", v, "\n"]
 
 doBinOp:: String -> String -> String -> String
-doBinOp op v1 v2 = "    " ++ op ++ " " ++ v1 ++ ", " ++ v2 ++ "\n"
+doBinOp op v1 v2 = concat ["    ", op, " ", v1, ", ", v2, "\n"]
 
 doUnOp:: String -> String -> String
-doUnOp op v = "    " ++ op ++ " " ++ v ++ "\n"
+doUnOp op v = concat ["    ", op, " ", v, "\n"]
 
 doOp:: String -> String
-doOp op = "    " ++ op ++ "\n"
+doOp op = concat ["    ", op, "\n"]
