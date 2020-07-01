7f9fdab30f0ff2b9edbb1cab7e55097933174ebe
DKarandikar <daniel.karandikar@zoho.com>
2020-07-01 23:20:50 +0100

    Update pretty printer for new nodes

--- src/compiler/PPrinter.hs
+++ src/compiler/PPrinter.hs
@@ -17,8 +17,15 @@ getIndent n = concat $ replicate n "    "
 strTree :: Int -> Tree -> String
 
 strTree n (ProgNode tree) = strTree n tree
-strTree n (FuncNode name trees) = "FUN " ++ name ++ ":\n" ++ (concat [strTree (n+1) tree ++ "\n" | tree <- trees])
-strTree n (ReturnNode tree) = (getIndent n) ++ "RETURN " ++ strTree n tree
-strTree n (ConstNode num) = "INT<" ++ (show num) ++ ">"
+strTree n (ExprNode tree) = strTree n tree
+strTree n (VarNode s) = s
+strTree n (AssignNode s tree) = s ++ " = " ++ (strTree n tree)
+strTree n (FuncNode name trees) = "FUN " ++ name ++ ":\n" ++ (concat [(getIndent (n+1)) ++ strTree (n+1) tree ++ "\n" | tree <- trees])
+strTree n (ReturnNode tree) = "RETURN " ++ strTree n tree
+strTree n (ConstNode num) = "<" ++ (show num) ++ ">"
 strTree n (UnOpNode s tree) = "(" ++ s ++ (strTree n tree) ++ ")"
 strTree n (BinOpNode s tree tree') = "(" ++ (strTree n tree) ++ s ++ (strTree n tree') ++ ")"
+strTree n (DeclNode s mayTree) = 
+    case mayTree of 
+        Just tree -> "INT " ++ s ++ " = " ++ (strTree n tree)
+        Nothing -> "INT " ++ s
