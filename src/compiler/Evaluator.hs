44e01909ccccd7d3f3a243c7b3acdd526cdf5122
DKarandikar <daniel.karandikar@zoho.com>
2020-07-05 00:24:25 +0100

    Implement evaluation rules to support local variables
    This now works for week 5 tests involving local variables
    Implement DeclNode, AssignNode + VarNode
    Fix implementation of FuncNode, i.e. mapM not head

--- src/compiler/Evaluator.hs
+++ src/compiler/Evaluator.hs
@@ -1,16 +1,20 @@
 module Evaluator(evaluate) where
 
 import Parser (Tree(..))
-import Control.Monad.State 
+import Control.Monad.State
+import qualified Data.Map as M
 
 rax = "%rax"
 rcx = "%rcx"
 
 evaluate :: Tree -> String
-evaluate tree = evalState (eval tree) 0
+evaluate tree = evalState (eval tree) (0, -8, M.empty)
 
 type IdCounter = Int
-type Evaluator = State IdCounter
+type StackIndex = Int
+type VariableMap = M.Map String Int
+type EvaluatorState = (IdCounter, StackIndex, VariableMap)
+type Evaluator = State EvaluatorState
 
 eval :: Tree -> Evaluator String
 
@@ -21,9 +25,9 @@ eval (ReturnNode tree) = do
     res <- eval tree
     return $ concat [res, move "%rbp" "%rsp", doUnOp "pop" "%rbp", "    ret"]  -- fn epilogue
 
-eval (FuncNode fname tree) = do
-    res <- eval $ head tree
-    return $ concat [".globl ", fname, "\n", fname, ":\n", doUnOp "push" "%rbp", move "%rsp" "%rbp", res]  --fn prologue
+eval (FuncNode fname trees) = do
+    ress <- mapM eval trees
+    return $ concat [".globl ", fname, "\n", fname, ":\n", doUnOp "push" "%rbp", move "%rsp" "%rbp", concat ress]  --fn prologue
 
 eval (UnOpNode op tree) = do 
     res <- eval tree 
@@ -32,6 +36,26 @@ eval (UnOpNode op tree) = do
         "~" -> concat [res, doUnOp "not" rax]
         "!" -> concat [res, doBinOp "cmpq" "$0" rax, move "$0" rax, doUnOp "sete" "%al"]
         _ -> error $ "Invalid unary operation: " ++ op
+
+eval (DeclNode varName maybeTree) = do
+    ensureVariableNotPresent varName
+    case maybeTree of 
+        Just v -> do
+            addSymbol varName
+            initialVal <- eval v
+            return $ concat [initialVal, push rax]
+        Nothing -> do
+            addSymbol varName
+            return $ concat [move "$0" rax, push rax]
+
+eval (AssignNode varName tree) = do
+    res <- eval tree
+    varLocation <- lookUp varName
+    return $ concat [res, move rax ((show varLocation) ++ "(%rbp)")]
+
+eval (VarNode varName) = do
+    varLocation <- lookUp varName
+    return $ move ((show varLocation) ++ "(%rbp)") rax
 
 eval (BinOpNode op tree tree') = do 
     lh_res <- eval tree 
@@ -58,10 +82,30 @@ eval (BinOpNode op tree tree') = do
         "&&" -> generateOrAndState generateAndAsm lh_res rh_res
         _  -> error $ "Invalid binary operation: " ++ op
 
+ensureVariableNotPresent :: String -> Evaluator ()
+ensureVariableNotPresent str = do
+    (_, _, map) <- get
+    case M.lookup str map of
+      Just v  -> error $ "Variable already defined: " ++ str
+      Nothing -> return ()
+
+lookUp :: String -> Evaluator Int
+lookUp str = do
+    (_, _, map) <- get
+    case M.lookup str map of
+      Just v  -> return v
+      Nothing -> error $ "Undefined variable " ++ str
+
+addSymbol :: String -> Evaluator ()
+addSymbol varName = do 
+    (c, stack, map) <- get
+    put $ (c, stack - 8, M.insert varName stack map)
+    return ()
+
 generateOrAndState :: (String -> String -> String -> String -> String) -> String -> String -> Evaluator String
 generateOrAndState asmGenerator lh_res rh_res = do
-    counter <- get
-    put (counter + 2)
+    (counter, s, m) <- get
+    put (counter + 2, s, m)
     let 
         id_1 = "_id" ++ (show counter)
         id_2 = "_id" ++ (show (counter + 1))
